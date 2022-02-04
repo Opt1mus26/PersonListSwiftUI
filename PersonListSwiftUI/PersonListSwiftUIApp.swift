f38b7b98047d57a28b7997e8f291dd99c98dc117
Pavel <92750573+Opt1mus26@users.noreply.github.com>
2022-02-04 12:54:16 +0300

    Initial Commit

---
+++ PersonListSwiftUI/PersonListSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  PersonListSwiftUIApp.swift
+//  PersonListSwiftUI
+//
+//  Created by Pavel Tsyganov on 04.02.2022.
+//
+
+import SwiftUI
+
+@main
+struct PersonListSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
