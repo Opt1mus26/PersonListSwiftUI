8563b05f2e842ea032ab3a4421b81582df8fa310
Pavel <92750573+Opt1mus26@users.noreply.github.com>
2022-02-05 13:28:29 +0300

    change code

--- PersonListSwiftUI/Detail.swift
+++ PersonListSwiftUI/Detail.swift
@@ -16,7 +16,7 @@ struct Detail: View {
                     Spacer()
                     Image(systemName: "person.fill")
                         .resizable()
-                        .frame(width: 200, height: 200, alignment: .center)
+                        .frame(width: 200, height: 200)
                     .padding()
                     Spacer()
                 }
